a4dbf0fe4a0f0458a38b7a7f99dcf6c5b5164898
Melyn Atieno Were <melynatieno@gmail.com>
2022-09-08 22:12:12 +0300

    Update new models

---
+++ app/models/users.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+    extend Memorable::ClassMethods
+    include Memorable::InstanceMethods
+  
+    validates_presence_of :name, :email, :country
+    validates :name, uniqueness: true
+    validates :email, uniqueness: true
+    
+    has_many :reviews
+    has_many :restaurants, through: :reviews
+    
+end
