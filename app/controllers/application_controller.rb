045a5186e3b5de8b69923fa37a4a39cf012d0ad8
Melyn Atieno Were <melynatieno@gmail.com>
2022-09-08 15:00:30 +0300

    create users migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
+require "./config/environment"
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+ 
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Hello, Voter!" }.to_json
   end
-
 end
+
+  #def user_session(session)
+    #@user = User.find_by_id(session[:user_id])
+
+#end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
-puts "🌱 Seeding spices..."
-
 # Seed your database here
+user = User.create({name:"John", email:"john@john.com", country:"Ethiopia"})
+user = User.create({name:"Felistus", email:"felistus@felistus.com", country:"Ghana"})
+user = User.create({name: "Andrew", email:"andrew@gmail.com", country:"UK"})
 
-puts "✅ Done seeding!"
+
+restaurant = Restaurant.create({name:"Teule", contact: "00204", location: "Nairobi"})
+restaurant = Restaurant.create({name:"Hella", contact:"45903", location:"Lagos"})
+restaurant = Restaurant.create({name:"Fille", contact:"34990", location:"New York"})
+
+rate = Rate.create({user_id: user.id, restaurant_id: restaurant.id, rating:"4"})
+rate = Rate.create({user_id: user.id, restaurant_id: restaurant.id, rating:"3"})
+rate = Rate.create({user_id: user.id, restaurant_id: restaurant.id, rating:"5"})
+
